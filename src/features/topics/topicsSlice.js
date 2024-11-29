551cb7691a9c72de42ca3cc7517ed9fae426948d
incursio16 <suhaimirizal@gmail.com>
2024-11-29 12:25:45 +0800

    start topicsSlice

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,10 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const topicsSlice = createSlice({
+    name: "topics",
+    initialState: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase()
+    }
+})
